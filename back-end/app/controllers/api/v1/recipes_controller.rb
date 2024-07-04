60d017786dbbd7e7dba533a2ba7c038e3d9b86ab
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-07-04 17:58:35 -0400

    feat: Add RecipesController for API v1

--- back-end/app/controllers/api/v1/recipes_controller.rb
+++ back-end/app/controllers/api/v1/recipes_controller.rb
@@ -1,34 +1,31 @@
 class Api::V1::RecipesController < ApplicationController
-  before_action :set_recipe, only: %i[show destroy]
-  def index
-    recipe = Recipe.all.order(created_at: :desc)
-    render json: recipe
+  before_action :set_recipe, only: [:show, :destroy]
+   # GET /api/v1/recipes
+   def index
+    @recipes = Recipe.all
+    render json: @recipes
   end
 
   def create
-    recipe = Recipe.create!(recipe_params)
-    if recipe
-      render json: recipe
-    else
-      render json: recipe.errors
-    end
   end
 
+  # GET /api/v1/recipes/1
   def show
     render json: @recipe
   end
 
+  # DELETE /api/v1/recipes/1
   def destroy
-    @recipe&.destroy
-    render json: { message: 'Recipe deleted!' }
+    @recipe.destroy
   end
-
   private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_recipe
+          @recipe = Recipe.find(params[:id])
+        end
 
-  def recipe_params
-    params.permit(:name, :image, :ingredients, :instruction)
-  end
-  def set_recipe
-    @recipe = Recipe.find(params[:id])
-  end
+        # Only allow a trusted parameter "white list" through.
+        def recipe_params
+          params.require(:recipe).permit(:title, :description, :instructions)
+        end
 end
--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "recipes/index"
-      post "recipes/create"
-      get '/show/:id', to: 'recipes#show'
-      delete '/destroy/:id', to: 'recipes#destroy'
+      resources :recipes, only: [:index, :show, :create, :destroy]
     end
   end
-
-  get '/*path' => 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -20,5 +15,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "homepage#index"
+  # root "posts#index"
 end
